0dca00c8cc987d5ffd818816c0133625c87aff4f
Roman Sommer <roman.sommer@fau.de>
2019-01-05 17:55:11 +0100

    backup-gen: get group names from pacman output

--- backup-gen.sh
+++ backup-gen.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 PROG="Backup Generator"
-VERSION="0.1.2"
+VERSION="0.1.3"
 
 DATE=$(date +%Y-%m-%d)
 NAME=$(hostname)-$DATE
@@ -13,33 +13,38 @@ echo "Erstelle Paketlisten ..."
 
 mkdir "$NAME/pkg"
 
-pacman -Qm > "$NAME/pkg/pkglist.aur"
-# echo ... falls pacman -Qqm ohne Ausgabe
-pacman -Qeq | grep -v "$(pacman -Sqg base base-devel xorg gnome gnome-extra kde libreoffice)" | grep -v "$(echo " " && pacman -Qqm)" > "$NAME/pkg/pkglist"
-pacman -Qqg xorg > "$NAME/pkg/pkglist.xorg"
-pacman -Qqg gnome > "$NAME/pkg/pkglist.gnome"
-pacman -Qqg gnome-extra > "$NAME/pkg/pkglist.gnome-extra"
-pacman -Qqg kde > "$NAME/pkg/pkglist.kde"
-pacman -Qqg libreoffice > "$NAME/pkg/pkglist.libreoffice"
+# find foreign packages
+pacman -Qm > "$NAME/pkg/aurpackages"
+
+# find explicityly installed repo packages sorted by groups
+pkggroups="$(pacman -Qg | cut -d' ' -f1 | sort -u)"
+echo "${pkggroups}" | while read -r pkggroup; do
+    pacman -Qeg "$pkggroup" | cut -d' ' -f2 > "$NAME/pkg/pkglist.$pkggroup"
+done
+
+# find explicitly installed packages from the repos not belonging to any group
+pacman -Qeq | grep -v "$(echo "${pkggroups}" | xargs -d'\n' pacman -Sqg)" | grep -v "$(pacman -Qmq || echo " ")" > "$NAME/pkg/pkglist"
+# enable this line to check tests
+#pacman -Qeq | grep -v "$(pacman -Qmq || echo " ")" > "$NAME/pkg/pkglist"
+
+# test output
+if [ "$(xargs -d '\n' -a "$NAME/pkg/pkglist" pacman -Qi | grep Groups | grep -v None -c)" -gt 0 ]; then
+    echo "Warning: there are packages in pkglist that may belong to a group." >&2
+fi
 
 echo "Sichere Systemkonfiguration ..."
 
 mkdir "$NAME/config"
-
 pacman -Qii | awk '/^MODIFIED/ {print $2}' | grep -v "shadow" >> "$NAME/config/configlist"
+
 # generate rsync commandline, $NAME/config/ is the target directory
 echo "$NAME/config/" | cat "$NAME/config/configlist" - | xargs -d '\n' rsync -R
-cp -r /etc/skel "$DATE/config/etc/"
+
+if [ -d /etc/skel ]; then
+cp -r /etc/skel "$NAME/config/etc/"
+fi
 
 echo "Komprimiere Daten ..."
-
-# lange her, hat nicht funktioniert, bleibt als reminder
-#echo "Erstelle Wiederherstellungsskript ..."
-#cat /dev/null >> $NAME/restore.sh
-#echo '#!/bin/sh' > $NAME/restore.sh
-#echo 'pacman -S $(cat pkg/pkglist)' > $NAME/restore.sh
-#echo 'cp --recursive /config/* /' > $NAME/restore.sh
-#chmod a+x $NAME/restore.sh
 
 tar -cJf "$NAME.tar.xz" "$NAME"/*
 rm -r "${NAME:?}/"
