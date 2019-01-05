07bd5460b9d5cbe1c82a5fd17be5b237250e0a76
Roman Sommer <roman.sommer@fau.de>
2019-01-05 16:09:08 +0100

    backup-gen: fix shellcheck warnings

--- backup-gen.sh
+++ backup-gen.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
 
 PROG="Backup Generator"
-VERSION="0.1.1"
+VERSION="0.1.2"
 
 DATE=$(date +%Y-%m-%d)
-NAME=`hostname`-$DATE
+NAME=$(hostname)-$DATE
 
-echo $PROG $VERSION
-mkdir -p $NAME/
+echo "$PROG" "$VERSION"
+mkdir -p "$NAME/"
 
 echo "Erstelle Paketlisten ..."
 
-mkdir $NAME/pkg
+mkdir "$NAME/pkg"
 
-pacman -Qm > $NAME/pkg/pkglist.aur
+pacman -Qm > "$NAME/pkg/pkglist.aur"
 # echo ... falls pacman -Qqm ohne Ausgabe
-pacman -Qeq | grep -v "$(pacman -Sqg base base-devel xorg gnome gnome-extra kde libreoffice)" | grep -v "$(echo " " && pacman -Qqm)" > $NAME/pkg/pkglist
-pacman -Qqg xorg > $NAME/pkg/pkglist.xorg
-pacman -Qqg gnome > $NAME/pkg/pkglist.gnome
-pacman -Qqg gnome-extra > $NAME/pkg/pkglist.gnome-extra
-pacman -Qqg kde > $NAME/pkg/pkglist.kde
-pacman -Qqg libreoffice > $NAME/pkg/pkglist.libreoffice
+pacman -Qeq | grep -v "$(pacman -Sqg base base-devel xorg gnome gnome-extra kde libreoffice)" | grep -v "$(echo " " && pacman -Qqm)" > "$NAME/pkg/pkglist"
+pacman -Qqg xorg > "$NAME/pkg/pkglist.xorg"
+pacman -Qqg gnome > "$NAME/pkg/pkglist.gnome"
+pacman -Qqg gnome-extra > "$NAME/pkg/pkglist.gnome-extra"
+pacman -Qqg kde > "$NAME/pkg/pkglist.kde"
+pacman -Qqg libreoffice > "$NAME/pkg/pkglist.libreoffice"
 
 echo "Sichere Systemkonfiguration ..."
 
-mkdir $NAME/config
+mkdir "$NAME/config"
 
-pacman -Qii | awk '/^MODIFIED/ {print $2}' | grep -v "shadow" >> $NAME/config/configlist
-rsync -R $(cat $NAME/config/configlist) $NAME/config/
-cp -r /etc/skel $DATE/config/etc/
+pacman -Qii | awk '/^MODIFIED/ {print $2}' | grep -v "shadow" >> "$NAME/config/configlist"
+# generate rsync commandline, $NAME/config/ is the target directory
+echo "$NAME/config/" | cat "$NAME/config/configlist" - | xargs -d '\n' rsync -R
+cp -r /etc/skel "$DATE/config/etc/"
 
 echo "Komprimiere Daten ..."
 
@@ -40,7 +41,7 @@ echo "Komprimiere Daten ..."
 #echo 'cp --recursive /config/* /' > $NAME/restore.sh
 #chmod a+x $NAME/restore.sh
 
-tar -cJf $NAME.tar.xz $NAME/*
-rm -r $NAME/
+tar -cJf "$NAME.tar.xz" "$NAME"/*
+rm -r "${NAME:?}/"
 
 echo "Fertig!"
